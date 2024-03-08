2ec27d84ba986722b53813bef3e3e9b0bffb00d9
Sean <43896344+Sean-PB@users.noreply.github.com>
2024-03-08 11:06:30 -0600

    Deleted and Moved Godot files

--- GodotProjects/Dodge the Creeps/Joystick_button.gd
+++
@@ -1,5 +0,0 @@
-extends TouchScreenButton
-
-func _inpput(event):
-	if event is InputEventScreenDrag or (event is InputEventScreenTouch and event.is_pressed()):
-		set_global_position(event.position)
